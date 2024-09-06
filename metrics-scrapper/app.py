c87f3f6fc064416c18b8bc7563a21556a2d480d2
Harit Sharma <hs80265@gmail.com>
2024-09-06 17:32:14 +0530

    Commit for docker challenege

---
+++ metrics-scrapper/app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask, jsonify
+import psutil
+
+app = Flask(__name__)
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    cpu = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().percent
+    return jsonify(cpu=cpu, memory=memory)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
